f6b56ee7bf1cff5d0d72aef102755fb2cad78d5d
Joan Cortes <js.199911@gmail.com>
2021-10-11 21:29:16 -0500

    Frontend - CRUD Sede (completo)

--- Ciclo 3/Asignacion de oficinas/AsigOficinas.App/AsigOficinas.App.Persistencia/Migrations/20211012022545_Entidades.Designer.cs
+++ Ciclo 3/Asignacion de oficinas/AsigOficinas.App/AsigOficinas.App.Persistencia/Migrations/20211012022545_Entidades.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AsigOficinas.App.Persistencia.Migrations
 {
     [DbContext(typeof(appContext))]
-    [Migration("20211011222923_Entidades")]
+    [Migration("20211012022545_Entidades")]
     partial class Entidades
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
