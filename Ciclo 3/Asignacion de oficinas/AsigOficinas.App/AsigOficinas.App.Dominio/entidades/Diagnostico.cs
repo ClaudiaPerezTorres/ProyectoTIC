6c5678563456d5a584b8a73557e3f9dc8c42ee38
Joan Cortes <js.199911@gmail.com>
2021-10-15 13:05:57 -0500

    Validaciones - Campo de fecha (completo)

--- Ciclo 3/Asignacion de oficinas/AsigOficinas.App/AsigOficinas.App.Dominio/entidades/Diagnostico.cs
+++ Ciclo 3/Asignacion de oficinas/AsigOficinas.App/AsigOficinas.App.Dominio/entidades/Diagnostico.cs
@@ -8,7 +8,8 @@ namespace AsigOficinas.App.Dominio
         public int id {get;set;}
         [Required(ErrorMessage = "Falta completar este campo"), StringLength(100, MinimumLength = 10)]
         public string sintomas {get;set;}
-        [Required(ErrorMessage = "Falta completar este campo")]
+        [Range(typeof(DateTime), "1/1/2021", "31/12/2050",
+        ErrorMessage = "La fecha debe estar {1} entre {2}")]
         public DateTime fechaDiagnostico {get;set;}
     }
 }
